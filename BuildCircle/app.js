5fe335bbb7535629531909a80cf2539e39a3d135
Mario <mario.garciapinan@rtgs.com>
2021-07-28 23:36:28 +0200

    First deploy to AWS

---
+++ BuildCircle/app.js
@@ -0,0 +1,20 @@
+let response;
+
+exports.buildCircleHandler = async (event, context) => {
+    let message = event.Records[0].Sns.Message
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: message,
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
