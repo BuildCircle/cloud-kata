76688067b1ca55bd3a6fbfbdaabe4975b27f357e
Mario <mario.garciapinan@rtgs.com>
2021-07-30 14:08:37 +0200

    modified structure

---
+++ aws/.aws-sam/build/BuildCircleHandler/app.js
@@ -0,0 +1,23 @@
+let response;
+
+exports.buildCircleHandler = async (event, context) => {
+    let message = event.Records[0].Sns.Message
+
+    console.log(message);
+    
+    try {
+        // const ret = await axios(url);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: message,
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
